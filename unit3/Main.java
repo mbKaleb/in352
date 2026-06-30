110b44606f8181865ed60b518a28e1d48ade87cd
Kaleb Franken <kaleb.franken@icloud.com>
2026-06-30 13:19:51 -0700

    final commit

--- unit3/Main.java
+++ unit3/Main.java
@@ -1,3 +1,4 @@
+
 //Main.java by Kaleb Franken
 import java.util.ArrayList;
 
@@ -11,15 +12,31 @@ public class Main {
         graph.printGraph();
 
         // Run Dijkstra's algorithm
-        // System.out.println("\n=== Dijkstra's Algorithm (A to D) ===");
-        // Dijkstra dijkstra = new Dijkstra(graph);
-        // ArrayList<Node> dijkstraPath = dijkstra.findShortestPath(graph.A, graph.D);
-        // dijkstra.printResult(dijkstraPath);
+        System.out.println("\n=== Dijkstra's Algorithm (A to D) ===");
+        Dijkstra dijkstra = new Dijkstra(graph);
+
+        long dijkstraStart = System.nanoTime();
+        ArrayList<Node> dijkstraPath = dijkstra.findShortestPath(graph.A, graph.D);
+        long dijkstraEnd = System.nanoTime();
+        long dijkstraTime = dijkstraEnd - dijkstraStart;
+
+        dijkstra.printResult(dijkstraPath);
+        System.out.println("Time: " + dijkstraTime + " nanoseconds (" + (dijkstraTime / 1_000_000.0) + " ms)");
 
         // Run Greedy Search
         System.out.println("\n=== Greedy Search (A to D) ===");
         GreedySearch greedy = new GreedySearch();
+
+        long greedyStart = System.nanoTime();
         ArrayList<Node> greedyPath = greedy.findPath(graph.A, graph.D);
+        long greedyEnd = System.nanoTime();
+        long greedyTime = greedyEnd - greedyStart;
+
         greedy.printResult(greedyPath);
+        System.out.println("Time: " + greedyTime + " nanoseconds (" + (greedyTime / 1_000_000.0) + " ms)");
+
+        System.out.println("Dijkstra's Time:   " + dijkstraTime + " ns");
+        System.out.println("Greedy Time:       " + greedyTime + " ns");
+
     }
 }
