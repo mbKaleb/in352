8bc2f99edc88dc3d126c1b6b2bb493d0b57d54cb
Kaleb Franken <kaleb.franken@icloud.com>
2026-08-16 23:36:35 -0700

    added some pages

---
+++ unit10/src/main/java/com/example/taskmanager/controller/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.example.taskmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.taskmanager.model.User;
+
+import com.example.taskmanager.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// handles incoming http requests and decides what happens in response
+@Controller //returns view for thymeleaft, not json, this is a server based rendering scheme
+public class RegistrationController {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public RegistrationController(PasswordEncoder pEncoder, UserRepository uRepo){
+        this.passwordEncoder = pEncoder;
+        this.userRepository = uRepo;
+        
+    }
+
+    @GetMapping("/register")
+    public String showRegisterForm() {
+        return "register";
+    }
+    
+    @PostMapping("/register") 
+    
+    public String postRegister(@RequestParam String  username, @RequestParam String password) {
+        
+        String encoded = passwordEncoder.encode(password);
+        User newUser = new User(username, encoded, "USER");
+        userRepository.save(newUser);
+        return "redirect:/login";
+    }
+}
