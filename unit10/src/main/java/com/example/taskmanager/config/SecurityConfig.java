2a7070f557d066b5ce5f95bdf8f862a275fcd88a
Kaleb Franken <kaleb.franken@icloud.com>
2026-08-16 20:31:10 -0700

    cleaned up old method added register endpoint

--- unit10/src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ unit10/src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package com.example.taskmanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.UserDetailsServiceConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -23,24 +18,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean 
-    public UserDetailsService details(PasswordEncoder pascoder) {
-        //Spring security interface defines getUsername, getPassword, getAuthorize, some status flags
-        UserDetails user = User.builder()
-            .username("user")
-            .password(pascoder.encode("password"))
-            .roles("USER") // Not an enum ;-;
-            .build(); //finalizes the object
-        
-        return new InMemoryUserDetailsManager(user); // associated user manager with in memory database
-    }
-
     @Bean  //define actual security policy, what urls need a login, what is public, what access level do we need?
     // This specific method right now says anyone can hit /login /css/** or /js/** */
     public SecurityFilterChain SFC(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/login", "/css/**","/js/**").permitAll();
+                auth.requestMatchers("/register", "/login", "/css/**","/js/**").permitAll();
                 auth.anyRequest().authenticated();
             })
             .formLogin(form -> {
